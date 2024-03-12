b269946a41d0137ffa1692f7706a190aca48fd47
Ralph Mpanu <rmpan001@odu.edu>
2024-03-11 20:27:16 -0400

    added JUnit testing imports

--- project/src/test/java/edu/odu/cs/cs350/testTextBlock.java
+++ project/src/test/java/edu/odu/cs/cs350/testTextBlock.java
@@ -1,3 +1,15 @@
+package edu.odu.cs.cs350
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 public class testTextBlock {
     
+
+    @test
+    public void testTextBlockConstructor()
+    {
+        TextBlock  inputTxt = new TextBlock(); 
+    }
 }
