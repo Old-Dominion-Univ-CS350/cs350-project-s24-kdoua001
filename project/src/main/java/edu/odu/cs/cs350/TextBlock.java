1ee8054f67ba17eb2b25ad5070228d19d73db74d
Izzy-Elizzy <izzyelhaimeurllc@gmail.com>
2024-03-11 20:24:00 -0400

    TextBlock Getters and Setters

--- project/src/main/java/edu/odu/cs/cs350/TextBlock.java
+++ project/src/main/java/edu/odu/cs/cs350/TextBlock.java
@@ -5,5 +5,11 @@ public class TextBlock {
         List<Tokens> Tokens;
     TextBlock(String BlockText){
 
-    } 
+    }
+    
+    String getTextBlock() {}
+    void setTextBlock(String AssignedBlock) {}
+
+    List<Tokens> getTokens() {}
+    void setTokens(List<Tokens> AssignedTokens)
 }
