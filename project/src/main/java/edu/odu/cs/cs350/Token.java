2655918ceba70c6882afee169d59040b269dd805
Peter-Spadaro <pspad001@odu.edu>
2024-03-11 20:54:27 -0400

    added features

--- project/src/main/java/edu/odu/cs/cs350/Token.java
+++ project/src/main/java/edu/odu/cs/cs350/Token.java
@@ -2,7 +2,12 @@ package edu.odu.cs.cs350;
 public class Token {
 
     private String theToken;
-    private List<Boolean> features;  
+    private boolean inDictionary;
+    private boolean isAPlace;
+    private booleen commonFirst;
+    private boolean commonLast;
+    private boolean honorific;
+    private boolean killWord;
     
     Token(String TokenString){
     }
