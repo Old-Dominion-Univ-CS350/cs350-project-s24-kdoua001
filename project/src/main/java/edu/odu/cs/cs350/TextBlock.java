2e1377ca0493497091695c842031e05250ba6a08
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-03-11 21:42:00 -0400

    Name Fix

--- project/src/main/java/edu/odu/cs/cs350/TextBlock.java
+++ project/src/main/java/edu/odu/cs/cs350/TextBlock.java
@@ -3,29 +3,13 @@ public class TextBlock {
     private:
         String TextBlock;
         List<Tokens> Tokens;
-        
+
     TextBlock(String BlockText){
         this.TextBlock = String BlockText;
     }
     
     public:
-        void setTextBlock(String AssignedBlock) {
-            this.TextBlock = String AssignedBlock;
-        }
 
-        String getTextBlock() {
-            return this.TextBlock;
-        }
-
-        void setTokens(List<Tokens> AssignedTokens) {
-            this.Tokens = List<Tokens> AssignedTokens;
-        }
-
-        List<Tokens> getTokens() {
-            return this.Tokens;
-        }
-
-        List<Tokens> createTokens(String BlockText) {}
 
     
 }
