5c3a89b868b8e53d22c27992f663255812ad617a
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-03-11 21:44:07 -0400

    Credit Fix

--- project/src/main/java/edu/odu/cs/cs350/Token.java
+++ project/src/main/java/edu/odu/cs/cs350/Token.java
@@ -9,7 +9,15 @@ public class Token {
     private boolean honorific;
     private boolean killWord;
     
-
+    Token(String AssignedString){
+        this.TokenString = String AssignedString;
+        this.inDictionary = false;
+        this.isLocation = false;
+        this.commonFirst = false;
+        this.commonLast = false;
+        this.honorific = false;
+        this.killWord = false;
+    }
 
     String getToken()
     {
