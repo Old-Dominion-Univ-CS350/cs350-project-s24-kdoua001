6feeb8a6fa8dcaf97346f957063ac24a6bad3b6c
Peter-Spadaro <pspad001@odu.edu>
2024-03-11 20:25:28 -0400

    added functions to TextBlock

--- project/src/main/java/edu/odu/cs/cs350/TextBlock.java
+++ project/src/main/java/edu/odu/cs/cs350/TextBlock.java
@@ -1,8 +1,30 @@
 public class TextBlock {
     private:
         String TextBlock;
+        List<Token> words;
 
     TextBlock(String BlockText){
         this 
     } 
+
+    public:
+        void setTextBlock(String theTextBlock)
+        {
+        }
+
+        String getTextBlock()
+        {
+            return this.TextBlock;
+        }
+
+        void setWords(TextBlock)
+        {
+        }
+
+        Token getWords()
+        {
+            return this.words;
+        }
+
+    
 }
