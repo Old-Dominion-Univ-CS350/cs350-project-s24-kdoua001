dd6c5515b06f513c059c77d9e74e07147f58e1a6
Izzy-Elizzy <izzyelhaimeurllc@gmail.com>
2024-03-11 20:19:43 -0400

    File Struct Fix

--- project/src/main/java/edu/odu/cs/cs350/TextBlock.java
+++ project/src/main/java/edu/odu/cs/cs350/TextBlock.java
@@ -1,8 +1,9 @@
+package edu.odu.cs.cs350;
 public class TextBlock {
     private:
         String TextBlock;
-
+        List<Tokens> Tokens;
     TextBlock(String BlockText){
-        this 
+
     } 
 }
