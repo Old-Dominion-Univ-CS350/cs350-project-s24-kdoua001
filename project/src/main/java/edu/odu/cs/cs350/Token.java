b71b94d95f2fe8ac2a1883b32623648907a227e5
Peter-Spadaro <pspad001@odu.edu>
2024-03-11 21:28:46 -0400

    added getters and setters

--- project/src/main/java/edu/odu/cs/cs350/Token.java
+++ project/src/main/java/edu/odu/cs/cs350/Token.java
@@ -1,17 +1,16 @@
 package edu.odu.cs.cs350;
 public class Token {
     
-    private:
-        String TokenString;
-        boolean inDictionary;
-        boolean isLocation;
-        boolean commonFirst;
-        boolean commonLast;
-        boolean honorific;
-        boolean killWord;
+    private String TokenString;
+    private boolean inDictionary;
+    private boolean isLocation;
+    private boolean commonFirst;
+    private boolean commonLast;
+    private boolean honorific;
+    private boolean killWord;
     
     Token(String AssignedString){
-        this.TokenString = String AssignedString;
+        this.TokenString = AssignedString;
         this.inDictionary = false;
         this.isLocation = false;
         this.commonFirst = false;
@@ -19,4 +18,75 @@ public class Token {
         this.honorific = false;
         this.killWord = false;
     }
+
+    String getToken()
+    {
+        return this.TokenString;
+    }
+
+    boolean getInDictionary()
+    {
+        return this.inDictionary;
+    }
+
+    boolean getIsLocation()
+    {
+        return this.isLocation;
+    }
+
+    boolean getCommonFirst()
+    {
+        return this.commonFirst;
+    }
+
+    boolean getCommonLast()
+    {
+        return this.commonLast;
+    }
+
+    boolean getHonorific()
+    {
+        return this.honorific;
+    }
+
+    boolean getKillWord()
+    {
+        return this.killWord;
+    }
+
+    void setToken(String str)
+    {
+        this.TokenString = str;
+    }
+
+    void setInDictonary(boolean bool)
+    {
+        this.inDictonary = bool;
+    }
+
+    void setIsLocation(boolean bool)
+    {
+        this.isLocation = bool;
+    }
+
+    void setCommonFirst(boolean bool)
+    {
+        this.commonFirst = bool;
+    }
+
+    void setCommonLast(boolean bool)
+    {
+        this.commonLast = bool;
+    }
+
+    void setHonorific(boolean bool)
+    {
+        this.honorific = bool;
+    }
+
+    void setKillWord(boolean bool)
+    {
+        this.killWord = bool;
+    }
+
 }
