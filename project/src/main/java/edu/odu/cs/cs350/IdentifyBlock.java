2104f6d6cce453bfa1e4a06e819c026ccde1a72e
Corey Brady <cbrad020@odu.edu>
2024-03-16 01:49:34 -0400

    added nerBlocks methods

--- project/src/main/java/edu/odu/cs/cs350/IdentifyBlock.java
+++ project/src/main/java/edu/odu/cs/cs350/IdentifyBlock.java
@@ -8,14 +8,18 @@ import java.io.InputStreamReader;
 public class IdentifyBlock {
    
     private String dataFromFile;
+    private List<String> nerBlocks;
    
-    //constructor
+    //Constructor
     public IdentifyBlock() {
         this.dataFromFile = "";
+        this.nerBlocks = new ArrayList<>();
     }
- 
+
+    //Constructor with data param
     public IdentifyBlock(String data) {
         this.dataFromFile = data;
+        this.nerBlocks = extractNerBlocks(data)
     }
  
     //getter for dataFromFile
@@ -23,6 +27,12 @@ public class IdentifyBlock {
         return this.dataFromFile;
     }
 
+    //Getter for nerBlocks
+    public List<String> getNerBlocks() {
+        return this.nerBlocks;
+    }
+
+    //Setter for dataFromFile
     public void setDataFromFile(String data){
         this.dataFromFile = data;
     }
@@ -37,121 +47,3 @@ public class IdentifyBlock {
    
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-public class IdentifyBlock {
-    //holds data from file
-    private String dataFromFile;
-    //holds list of strings
-    private List<String> nerBlocks;
-    
-    //Default constructor 
-    public IdentifyBlock() {
-        this.dataFromFile = "";
-        this.nerBlocks = new ArrayList<>();
-    }
-
-    //Data paramater contructor 
-    public IdentifyBlock(String data) {
-        this.dataFromFile = data;
-        this.nerBlocks = extractNerBlocks(data);
-    }
-
-     //Getter for datFromFile
-     public String getDataFromFile() {
-        return this.dataFromFile;
-    }
-    
-    //Getter for nerBlocks
-    public List<String> getNerBlocks() {
-        return this.nerBlocks;
-    }
-
-    //Overide toString method to return dataFromFile
-    @Override
-    public String toString() {
-        return this.dataFromFile;
-    }
-
-    //Reads content of file 
-    public String readFile(String filePath) {
-        StringBuilder content = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return content.toString();
-    }
-
-    public static final String NER_OPEN = "<NER>";
-    public static final String NER_CLOSE = "</NER>";
-    public static final int NER_LAST = 6;
-
-    private bool ValidTags(String data, int index)
-    {
-        return (data.indexOf(NER_OPEN, index) >= 0 && data.indexOf(NER_CLOSE, index) >= 0) ? true : false;
-    }
-
-    private int CloseIndex(String data, int index)
-    {
-        return data.indexOf(NER_CLOSE, index) + NER_LAST;
-    }
-
-    //Extracts text blocks from data
-    private List<String> extractNerBlocks(String data) {
-        List<String> blocks = new ArrayList<>();
-        int index = data.indexOf(NER_OPEN, 0);
-        int tempIndex;
-
-        while (ValidTags(data, index)) {
-            tempIndex = index;
-            index += CloseIndex(data, tempIndex);
-            blocks.add(data.substring(tempIndex, index)); 
-            index++;
-        } 
-        return blocks;
-    }
- 
-} 
-*/
--- project/src/test/java/edu/odu/cs/cs350/IdentifyBlockTest.java
+++ project/src/test/java/edu/odu/cs/cs350/IdentifyBlockTest.java
@@ -1,4 +1,5 @@
 package edu.odu.cs.cs350;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
