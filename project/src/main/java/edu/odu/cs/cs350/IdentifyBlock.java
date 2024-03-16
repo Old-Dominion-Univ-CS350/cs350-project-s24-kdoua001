78cd722d8ccfc5fd9c41d19e446ba879b221e3ea
Corey Brady <cbrad020@odu.edu>
2024-03-16 01:33:15 -0400

    Fixed file reader methods

--- project/src/main/java/edu/odu/cs/cs350/IdentifyBlock.java
+++ project/src/main/java/edu/odu/cs/cs350/IdentifyBlock.java
@@ -1,11 +1,81 @@
 package edu.odu.cs.cs350;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
+import java.io.InputStreamReader;
 
+
+ 
+public class IdentifyBlock {
+   
+    private String dataFromFile;
+   
+    //constructor
+    public IdentifyBlock() {
+        this.dataFromFile = "";
+    }
+ 
+    public IdentifyBlock(String data) {
+        this.dataFromFile = data;
+    }
+ 
+    //getter for dataFromFile
+    public String getDataFromFile(){
+        return this.dataFromFile;
+    }
+
+    public void setDataFromFile(String data){
+        this.dataFromFile = data;
+    }
+
+    public void readInput() throws Exception {
+        Scanner scanner = new Scanner(new InputStreamReader(System.in, "UTF-8"));
+        scanner.useDelimiter("\\A");
+        String content = scanner.next();
+        this.dataFromFile = content;
+        scanner.close();
+    }
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 public class IdentifyBlock {
     //holds data from file
     private String dataFromFile;
@@ -24,8 +94,24 @@ public class IdentifyBlock {
         this.nerBlocks = extractNerBlocks(data);
     }
 
+     //Getter for datFromFile
+     public String getDataFromFile() {
+        return this.dataFromFile;
+    }
+    
+    //Getter for nerBlocks
+    public List<String> getNerBlocks() {
+        return this.nerBlocks;
+    }
+
+    //Overide toString method to return dataFromFile
+    @Override
+    public String toString() {
+        return this.dataFromFile;
+    }
+
     //Reads content of file 
-    private String readFile(String filePath) {
+    public String readFile(String filePath) {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
@@ -38,35 +124,34 @@ public class IdentifyBlock {
         return content.toString();
     }
 
+    public static final String NER_OPEN = "<NER>";
+    public static final String NER_CLOSE = "</NER>";
+    public static final int NER_LAST = 6;
+
+    private bool ValidTags(String data, int index)
+    {
+        return (data.indexOf(NER_OPEN, index) >= 0 && data.indexOf(NER_CLOSE, index) >= 0) ? true : false;
+    }
+
+    private int CloseIndex(String data, int index)
+    {
+        return data.indexOf(NER_CLOSE, index) + NER_LAST;
+    }
+
     //Extracts text blocks from data
     private List<String> extractNerBlocks(String data) {
         List<String> blocks = new ArrayList<>();
-        int startIndex = 0;
-        while (true) {
-            int nerStartIndex = data.indexOf("<NER>", startIndex);
-            if (nerStartIndex == -1) break;
-            int nerEndIndex = data.indexOf("</NER>", nerStartIndex);
-            if (nerEndIndex == -1) break;
-            blocks.add(data.substring(nerStartIndex, nerEndIndex + 6)); 
-            startIndex = nerEndIndex + 6;
+        int index = data.indexOf(NER_OPEN, 0);
+        int tempIndex;
+
+        while (ValidTags(data, index)) {
+            tempIndex = index;
+            index += CloseIndex(data, tempIndex);
+            blocks.add(data.substring(tempIndex, index)); 
+            index++;
         } 
         return blocks;
     }
-
-    //Getter for datFromFile
-    public String getDataFromFile() {
-        return this.dataFromFile;
-    }
-    
-    //Getter for nerBlocks
-    public List<String> getNerBlocks() {
-        return this.nerBlocks;
-    }
-
-    //Overide toString method to return dataFromFile
-    @Overide
-    public String toString() {
-        return this.dataFromFile;
-    }
  
 } 
+*/
