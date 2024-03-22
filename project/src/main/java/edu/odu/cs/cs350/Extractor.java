082b4959d765b71848503b9801d194b53b4979da
rmpan001 <rmpan001@odu.edu>
2024-03-22 19:01:57 -0400

    Update Extractor.java
    //uncommented lines of code

--- project/src/main/java/edu/odu/cs/cs350/Extractor.java
+++ project/src/main/java/edu/odu/cs/cs350/Extractor.java
@@ -6,66 +6,54 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * Class for running and outputting program
- */
 public class Extractor {
     // private String input;
-    
-    /*
+
     private String dataFromTextBlocks;
 
-    public void getDataFromTextBlocks()
-    {
+    public void getDataFromTextBlocks() {
         this.dataFromTextBlocks = "";
     }
 
-    public void getDataFromTextBlocks(String theString)
-    {
+    public void getDataFromTextBlocks(String theString) {
         this.dataFromTextBlocks = theString;
     }
 
-    public void outputText()
-    {
+    public void outputText() {
         OutputInfo newOutput = new OutputInfo();
         String output = newOutput.getDataFromTextBlocks();
         System.out.println(output);
     }
-    */
 
     /**
      * Main function
      */
-    public void Main()
-    {
-        /*
+    public void Main() {
+
         // To generate the list of blocks
-        // List<String> nerBlocks;
-        
+        List<String> nerBlocks;
+
         // Create output list
-        // List<String> output = new ArrayList<>();
+        List<String> output = new ArrayList<>();
 
         // Create list of blocks
-        // IdentifyBlock inputBlock = new IdentifyBlock();
-        
+        IdentifyBlock inputBlock = new IdentifyBlock();
+
         // Get text blocks
         nerBlocks = inputBlock.getNerBlocks();
 
         // Loop for adding blocks to output
-        for (String string : nerBlocks)
-        {
+        for (String string : nerBlocks) {
             // Get text block to add to output
             TextBlock textBlock = new TextBlock(string);
             // Add text block to output list
             output.add(textBlock.toString());
         }
         // Loop for outputting textblocks
-        for (String string : output)
-        {
+        for (String string : output) {
             // Output each text block from output list
             System.out.println(string);
         }
-        */
+
     }
 }
