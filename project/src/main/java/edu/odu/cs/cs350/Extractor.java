fffcc8a9b2053677f92ceea4865e5efeea6a10df
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-03-22 20:30:01 -0400

    Expection Upper Case

--- project/src/main/java/edu/odu/cs/cs350/Extractor.java
+++ project/src/main/java/edu/odu/cs/cs350/Extractor.java
@@ -3,7 +3,7 @@ package edu.odu.cs.cs350;
 
 public class Extractor {
 
-    public static void main(String[] args) throws exception {
+    public static void main(String[] args) throws Exception {
         if (args.length == 0) {
             System.out.println("Usage: java Main <command> <inputFile>");
             return;
