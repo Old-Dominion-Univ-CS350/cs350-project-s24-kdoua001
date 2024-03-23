b4832c5d14d6201ca2f71c07c6b220e7ea9cf294
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-03-22 20:13:05 -0400

    Empty Arg Check

--- project/src/main/java/edu/odu/cs/cs350/Extractor.java
+++ project/src/main/java/edu/odu/cs/cs350/Extractor.java
@@ -3,6 +3,11 @@ package edu.odu.cs.cs350;
 
 public class Extractor {
 
-    public void Main() {}
+    public void Main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: java Main <command> <inputFile>");
+            return;
+        }
+    }
 
 }
