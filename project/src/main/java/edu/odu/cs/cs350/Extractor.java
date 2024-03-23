9741a0cc2cc55065e6631037bccb7112b036ffdd
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-03-22 20:29:12 -0400

    Throws Exepction Main

--- project/src/main/java/edu/odu/cs/cs350/Extractor.java
+++ project/src/main/java/edu/odu/cs/cs350/Extractor.java
@@ -3,7 +3,7 @@ package edu.odu.cs.cs350;
 
 public class Extractor {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws exception {
         if (args.length == 0) {
             System.out.println("Usage: java Main <command> <inputFile>");
             return;
