4d16aaa75e7421a3e90fca8836e328ad8c4f6ad9
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-03-22 21:49:37 -0400

    Check Statement Fix

--- project/src/main/java/edu/odu/cs/cs350/Extractor.java
+++ project/src/main/java/edu/odu/cs/cs350/Extractor.java
@@ -5,22 +5,22 @@ import java.io.File;
 public class Extractor {
 
     public static void main(String[] args) throws Exception {
-        if (args.length == 0) {
-            System.out.println("Usage: java Main <command> <inputFile>");
-            // return;
-        } else {
-
-            String inputFile = args[0];
-
-            File file = new File(inputFile);
-            if (!file.exists()) {
-                System.out.println("Error: Input file not found.");
-                return;
-            }
-
-            System.out.println("Input file: " + inputFile);
+        if (args.length != 1) {
+            System.out.println("Usage: java Extractor <inputFile>");
+            return;
         }
 
-    }
+        String inputFile = args[0];
 
+        // Check if the input file exists
+        File file = new File(inputFile);
+        if (!file.exists()) {
+            System.out.println("Error: Input file not found.");
+            return;
+        }
+
+        System.out.println("Input file: " + inputFile);
+
+        // Perform actions with the input file
+    }
 }
