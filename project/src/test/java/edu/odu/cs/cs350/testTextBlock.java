193fa960ab30cc00fc15698d71cd6d24fdbcac4b
rmpan001 <rmpan001@odu.edu>
2024-03-30 21:11:15 -0400

    Update testTextBlock.java
    starting tdd process for lexical features

--- project/src/test/java/edu/odu/cs/cs350/testTextBlock.java
+++ project/src/test/java/edu/odu/cs/cs350/testTextBlock.java
@@ -55,13 +55,13 @@ public class testTextBlock {
         String text = "<NER>My name is John Doe!</NER>";
         TextBlock block = new TextBlock(text);
         assertThat(block.toString(), is("<NER>My name is <PER>John Doe</PER>!</NER>"));
-        
+
         Token John = new Token("John");
         John.setIsName(true);
         John.setIsPunctuation(false);
         Token Jim = new Token("Jim");
-        Jim.setIsName(true);  
-        Jim.setIsPunctuation(false);      
+        Jim.setIsName(true);
+        Jim.setIsPunctuation(false);
         Token Doe = new Token("Doe");
         Doe.setIsName(true);
         Doe.setIsPunctuation(false);
@@ -79,7 +79,7 @@ public class testTextBlock {
     }
 
     @Test
-    public void testMyNameIs(){
+    public void testMyNameIs() {
         TextBlock textBlock = new TextBlock("<NER>My name is John Doe!</NER>");
         assertTrue(textBlock.containsString("My"));
         textBlock.myNameIs();
@@ -87,14 +87,44 @@ public class testTextBlock {
     }
 
     @Test
-    public void testGenerateShingles(){
+    public void testGenerateShingles() {
         List<List<Token>> shingles = new ArrayList<>();
         String text = "<NER>by John Doe, Lawrence Livermore Laboratory n</NER>";
         TextBlock block = new TextBlock(text);
         shingles = block.generateShingles(block.getTokensList());
-        for(List<Token> shingle : shingles){
+        for (List<Token> shingle : shingles) {
             System.out.println("Shingle: " + shingle);
         }
+    }
+
+    @Test
+    public void testIsLexicalFeature() {
+        String lexicalFeaturesStringExample = "This is test input 10";
+        TextBlock aBlockOfText = new TextBlock(lexicalFeaturesStringExample);
+        boolean isANum = false;
+        List<Token> inputTokens = aBlockOfText.createTokens(lexicalFeaturesStringExample);
+
+        for (int i = 0; i < inputTokens.size(); i++) {
+            Token token = inputTokens.get(i);
+            String tokenValue = token.getTokenString();
+            if (tokenValue.matches("\\d+")) {
+                isANum = true;
+
+                break;
+            }
+        }
+        // for (Token token : inputTokens) {
+        // String tokenValue = token.getTokenString();
+        // if (tokenValue.matches("\\d+")) {
+        // isANum = true;
+
+        // break;
+        // }
+
+        // }
+
+        assertTrue(isANum);
+
     }
 
 }
