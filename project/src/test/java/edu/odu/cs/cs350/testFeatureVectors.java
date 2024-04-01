bbd7fd74c5b2182d333dc65dd16b9a912a0311a8
Corey Brady <cbrad020@odu.edu>
2024-04-01 18:31:05 -0400

    add testFeatureVectors class

---
+++ project/src/test/java/edu/odu/cs/cs350/testFeatureVectors.java
@@ -0,0 +1,19 @@
+package edu.odu.cs.cs350;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class testFeatureVectors {
+
+    
+    
+}
