4f3773bdab14e3e9bfe6e57a3b15b4c0fb94ad5f
Corey Brady <cbrad020@odu.edu>
2024-04-04 21:53:08 -0400

    added test for creat vectors

--- project/src/test/java/edu/odu/cs/cs350/testFeatureVectors.java
+++ project/src/test/java/edu/odu/cs/cs350/testFeatureVectors.java
@@ -1,19 +1,69 @@
 package edu.odu.cs.cs350;
 
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import weka.core.Attribute;
-import weka.core.DenseInstance;
 import weka.core.Instances;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 public class testFeatureVectors {
 
+    @Test
+    public void testCreateVectors() {
+        //Create a list of sample tokens
+        List<Token> tokenList = new ArrayList<>();
+        tokenList.add(createToken(true, true, true, true, true, true, true, true, LexicalFeature.NUMBER));
+        tokenList.add(createToken(false, true, false, false, false, false, false, false, LexicalFeature.ALLCAPS));
+
+        //Create the FeatureVector instance
+        FeatureVector featureVector = new FeatureVector();
+        Instances data = featureVector.createVectors(tokenList);
+
+        //Assertions
+        assertNotNull(data);
+        assertEquals(tokenList.size(), data.numInstances());
+
+        //Check attribute values for the first instance
+        double[] instanceValues = data.get(0).toDoubleArray();
+        assertEquals(1.0, instanceValues[0]); // isName
+        assertEquals(1.0, instanceValues[1]); // isInDictionary
+        assertEquals(1.0, instanceValues[2]); // isLocation
+        assertEquals(1.0, instanceValues[3]); // isCommonFirst
+        assertEquals(1.0, instanceValues[4]); // isCommonLast
+        assertEquals(1.0, instanceValues[5]); // isHonorific
+        assertEquals(1.0, instanceValues[6]); // isKillWord
+        assertEquals(1.0, instanceValues[7]); // isPunctuation
+        assertEquals(LexicalFeature.NUMBER.ordinal(), (int) instanceValues[8]); // lexicalFeature
+
+        //Check attribute values for the second instance
+        instanceValues = data.get(1).toDoubleArray();
+        assertEquals(0.0, instanceValues[0]); // isName
+        assertEquals(1.0, instanceValues[1]); // isInDictionary
+        assertEquals(0.0, instanceValues[2]); // isLocation
+        assertEquals(0.0, instanceValues[3]); // isCommonFirst
+        assertEquals(0.0, instanceValues[4]); // isCommonLast
+        assertEquals(0.0, instanceValues[5]); // isHonorific
+        assertEquals(0.0, instanceValues[6]); // isKillWord
+        assertEquals(0.0, instanceValues[7]); // isPunctuation
+        assertEquals(LexicalFeature.ALLCAPS.ordinal(), (int) instanceValues[8]); // lexicalFeature
+    }
+
+    //Method to create a mock Token object
+    private Token createToken(boolean name, boolean inDictionary, boolean location, boolean commonFirst,
+                              boolean commonLast, boolean honorific, boolean killWord, boolean punctuation,
+                              LexicalFeature lexicalFeature) {
+        Token token = new Token("", lexicalFeature);
+        token.setIsName(name);
+        token.setIsInDictionary(inDictionary);
+        token.setIsLocation(location);
+        token.setIsCommonFirst(commonFirst);
+        token.setIsCommonLast(commonLast);
+        token.setIsHonorific(honorific);
+        token.setIsKillWord(killWord);
+        token.setIsPunctuation(punctuation);
+        return token;
+    }
     
     
 }
