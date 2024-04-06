51b19e36a666578630136af7737889fff5b74a06
Khamnulin Douangdara <kdoua001@odu.edu>
2024-04-06 19:58:46 -0400

    edited extractor and svmtrainer

--- project/src/main/java/edu/odu/cs/cs350/SVMtrainer.java
+++ project/src/main/java/edu/odu/cs/cs350/SVMtrainer.java
@@ -5,7 +5,10 @@ import weka.classifiers.functions.SMO;
 import weka.classifiers.functions.supportVector.RBFKernel;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.WekaPackageClassLoaderManager;
+
 import java.io.*;
+import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -23,37 +26,39 @@ public class SVMtrainer {
      */
     public SMO trainSVM(Instances data) throws Exception {
 
+        data.setClassIndex(data.numAttributes() - 1);
         String[] options = { "-N", "0", "-V", "-1" };
 
         // Tuning hyperparameters gamma and C
-        double bestGamma = 0.01;
-        double bestC = 8.0;
-        double bestResult = 0.0;
-
-        for (double gamma = 0.01; gamma <= 0.1; gamma += 0.09 / 7) {
-            for (double c = 8.0; c <= 15.0; c += 7 / 7) {
-                SMO svm = new SMO();
-                svm.setOptions(options);
-                svm.setKernel(new RBFKernel(data, 25007, gamma));
-                svm.setC(c);
-
-                Evaluation eval = new Evaluation(data);
-                eval.crossValidateModel(svm, data, 5, new java.util.Random(1));
-                double score = eval.pctCorrect();
-
-                if (score > bestResult) {
-                    bestResult = score;
-                    bestGamma = gamma;
-                    bestC = c;
-                }
-            }
-        }
-
+        /*
+         * double bestGamma = 0.01;
+         * double bestC = 8.0;
+         * double bestResult = 0.0;
+         * 
+         * for (double gamma = 0.01; gamma <= 0.1; gamma += 0.09 / 7) {
+         * for (double c = 8.0; c <= 15.0; c += 7 / 7) {
+         * SMO svm = new SMO();
+         * svm.setOptions(options);
+         * svm.setKernel(new RBFKernel(data, 25007, gamma));
+         * svm.setC(c);
+         * 
+         * Evaluation eval = new Evaluation(data);
+         * eval.crossValidateModel(svm, data, 5, new java.util.Random(1));
+         * double score = eval.pctCorrect();
+         * 
+         * if (score > bestResult) {
+         * bestResult = score;
+         * bestGamma = gamma;
+         * bestC = c;
+         * }
+         * }
+         * }
+         */
         // Create SMO (Sequential Minimal Optimization) classifier with best parameters
         SMO svm = new SMO();
         svm.setOptions(options);
-        svm.setKernel(new RBFKernel(data, 25007, bestGamma));
-        svm.setC(bestC);
+        // svm.setKernel(new RBFKernel(data, 25007, bestGamma));
+        // svm.setC(bestC);
 
         // Train it
         svm.buildClassifier(data);
