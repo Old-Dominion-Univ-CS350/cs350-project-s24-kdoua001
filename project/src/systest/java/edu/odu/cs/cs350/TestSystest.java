f0498bf88ab17e664f2cf0a220d1b67d7e4f0f5a
Peter-Spadaro <pspad001@odu.edu>
2024-04-20 12:48:35 -0400

    Fixed issues with system tests

---
+++ project/src/systest/java/edu/odu/cs/cs350/TestSystest.java
@@ -0,0 +1,14 @@
+package edu.odu.cs.cs350;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSystest {
+
+    @Test 
+    public void easyTest(){
+        assertEquals(1,1);
+    }
+    
+}
