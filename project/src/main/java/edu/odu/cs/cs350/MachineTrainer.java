c67469381648bc50c0750b2fbb4cee5330fb63f3
Corey Brady <cbrad020@odu.edu>
2024-04-21 16:39:13 -0400

    Changed entire MachineTrainer.java class

--- project/src/main/java/edu/odu/cs/cs350/MachineTrainer.java
+++ project/src/main/java/edu/odu/cs/cs350/MachineTrainer.java
@@ -1,83 +1,126 @@
 package edu.odu.cs.cs350;
 
-import weka.classifiers.Evaluation;
 import weka.classifiers.functions.SMO;
-import weka.classifiers.functions.supportVector.RBFKernel;
-import weka.core.Instance;
+import weka.classifiers.Evaluation;
 import weka.core.Instances;
-import weka.core.WekaPackageClassLoaderManager;
-
-import java.io.*;
-import java.lang.reflect.Method;
+import weka.core.SerializationHelper;
+import weka.core.Utils;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.List;
+import java.util.Random;
 
 /**
- * This class provides methods for training and saving Support Vector Machine
- * (SVM) models using WEKA.
+ * Trains a machine learning model using the Weka library and saves the trained model.
  */
 public class MachineTrainer {
 
     /**
-     * Trains a Support Vector Machine (SVM) model.
+     * Trains the SMO model using the specified data file and saves the trained model to the given path.
      *
-     * @param data The dataset to be used for training.
-     * @return The trained SVM model.
-     * @throws Exception If an error occurs during training.
+     * @param filePath The path to the training data file.
+     * @param modelPath The path where the trained model will be saved.
+     * @throws Exception if there is an error during the training or saving process.
      */
-    public SMO trainSupportVectorMachine(Instances data) throws Exception {
+    public void trainAndSaveModel(String filePath, String modelPath) throws Exception {
+        Document document = new Document(readFileContent(filePath));
+        List<TextBlock> blocks = document.getBlocks();
 
-        data.setClassIndex(data.numAttributes() - 1);
-        String[] options = { "-N", "0", "-V", "-1" };
+        FeatureVector featureVector = new FeatureVector();
+        Instances data = null;
 
-        // Tuning hyperparameters gamma and C
-        /*
-         * double bestGamma = 0.01;
-         * double bestC = 8.0;
-         * double bestResult = 0.0;s
-         * 
-         * for (double gamma = 0.01; gamma <= 0.1; gamma += 0.09 / 7) {
-         * for (double c = 8.0; c <= 15.0; c += 7 / 7) {
-         * SMO svm = new SMO();
-         * svm.setOptions(options);
-         * svm.setKernel(new RBFKernel(data, 25007, gamma));
-         * svm.setC(c);
-         * 
-         * Evaluation eval = new Evaluation(data);
-         * eval.crossValidateModel(svm, data, 5, new java.util.Random(1));
-         * double score = eval.pctCorrect();
-         * 
-         * if (score > bestResult) {
-         * bestResult = score;
-         * bestGamma = gamma;
-         * bestC = c;
-         * }
-         * }
-         * }
-         */
-        // Create SMO (Sequential Minimal Optimization) classifier with best parameters
-        SMO SupportVectorMachine = new SMO();
-        SupportVectorMachine.setOptions(options);
-        // svm.setKernel(new RBFKernel(data, 25007, bestGamma));
-        // svm.setC(bestC);
+        for (TextBlock block : blocks) {
+            List<Token> tokens = block.getTokensList();
+            if (data == null) {
+                data = featureVector.createVectors(tokens);
+                data.setClassIndex(data.numAttributes() - 1);
+            }
+            Instances newData = featureVector.createVectors(tokens);
+            for (int i = 0; i < newData.numInstances(); i++) {
+                data.add(newData.instance(i));
+            }
+        }
 
-        // Train it
-        SupportVectorMachine.buildClassifier(data);
-
-        return SupportVectorMachine;
+        SMO bestSmo = tuneParameters(data);
+        saveModel(bestSmo, modelPath);
     }
 
     /**
-     * Saves the trained SVM model to a file.
+     * Tunes the SMO classifier by testing different configurations and selecting the one with the highest accuracy.
      *
-     * @param SupportVectorMachine The trained SVM model to be saved.
-     * @param modelFile            The file path where the model will be saved.
-     * @throws Exception If an error occurs during model saving.
+     * @param data The dataset to be used for tuning the model.
+     * @return The best tuned SMO model.
+     * @throws Exception if there is an error during the model evaluation.
      */
-    public void saveModel(SMO SupportVectorMachine, String modelFile) throws Exception {
-        // Save the trained model to a file
-        ObjectOutputStream Output = new ObjectOutputStream(new FileOutputStream(modelFile));
-        Output.writeObject(SupportVectorMachine);
-        Output.close();
+    private SMO tuneParameters(Instances data) throws Exception {
+        double bestC = 1.0;
+        double bestResult = 0.0;
+
+        for (double c = 1.0; c <= 10.0; c += 1.0) {
+            SMO smo = new SMO();
+            smo.setC(c);
+            smo.setOptions(Utils.splitOptions("-L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1"));
+
+            Evaluation eval = new Evaluation(data);
+            eval.crossValidateModel(smo, data, 5, new Random(1));
+            double score = eval.pctCorrect();
+
+            if (score > bestResult) {
+                bestResult = score;
+                bestC = c;
+            }
+        }
+
+        SMO smo = new SMO();
+        smo.setC(bestC);
+        smo.setOptions(Utils.splitOptions("-L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1"));
+        smo.buildClassifier(data);
+        return smo;
     }
 
+    /**
+     * Reads the entire content of a file into a string.
+     *
+     * @param filePath The path to the file to be read.
+     * @return The content of the file.
+     * @throws FileNotFoundException if the file cannot be found.
+     */
+    String readFileContent(String filePath) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(filePath));
+        scanner.useDelimiter("\\A");
+        String content = scanner.next();
+        scanner.close();
+        return content;
+    }
+
+    /**
+     * Saves the trained model to the specified file path.
+     *
+     * @param smo The SMO model to be saved.
+     * @param filePath The path where the model will be saved.
+     * @throws Exception if there is an error during the saving process.
+     */
+    void saveModel(SMO smo, String filePath) throws Exception {
+        File file = new File(filePath);
+        file.getParentFile().mkdirs(); // Ensure the directory exists
+        SerializationHelper.write(filePath, smo);
+    }
+
+    /**
+     * The main method that runs the training process.
+     *
+     * @param args Command-line arguments.
+     */
+    public static void main(String[] args) {
+        try {
+            String trainingDataPath = "src/main/data/trainingData.txt";
+            String modelPath = "src/main/resources/model/smo.model";
+            new MachineTrainer().trainAndSaveModel(trainingDataPath, modelPath);
+            System.out.println("Model trained and saved successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to train or save the model.");
+        }
+    }
 }
