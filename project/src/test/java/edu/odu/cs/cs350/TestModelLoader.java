58de34f175478151fe7b13b8df397e06448e0d49
Corey Brady <cbrad020@odu.edu>
2024-04-22 21:46:58 -0400

    added test

---
+++ project/src/test/java/edu/odu/cs/cs350/TestModelLoader.java
@@ -0,0 +1,17 @@
+package edu.odu.cs.cs350;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestModelLoader {
+
+    @Test
+    public void testLoadModelFromJar() {
+        try {
+            // This is a placeholder test that always passes essentially testing basic functionality.
+            assertTrue(true, "This test will always pass.");
+        } catch (Exception e) {
+            fail("This test is designed to never fail.");
+        }
+    }
+}
